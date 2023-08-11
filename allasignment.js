a7b33d4f9ad94fac83b062ea4ac3d42af6a5e700
SuronjoyRoy <suronjoyroy21@gmail.com>
2023-08-11 20:36:58 +0600

    final commit

--- allasignment.js
+++ allasignment.js
@@ -1,3 +1,4 @@
+// Problems first
 function cubeNumber(number){
     if(typeof number !=='number'){
        return "Please enter a number" 
@@ -5,6 +6,12 @@ function cubeNumber(number){
     return number*number*number;
 }
 
+const number=4;
+const numberResult=cubeNumber(number);
+console.log(numberResult);
+
+
+// Problems second
 
 function matchFinder(string1, string2) {
     if (typeof string1 !== 'string' || typeof string2 !== 'string') {
@@ -17,6 +24,14 @@ function matchFinder(string1, string2) {
     return false;
 }
 
+console.log(matchFinder("John Doe", "ohn")); 
+console.log(matchFinder("JavaScript", "Code")); 
+console.log(matchFinder("Peter Parker", "Pen")); 
+console.log(matchFinder("Peter Parker", "pet")); 
+console.log(matchFinder(123, "abc"));
+
+
+// Problems thired
 
 function sortMaker(arr) {
     
@@ -37,7 +52,10 @@ function sortMaker(arr) {
     return arr.slice().sort((a, b) => b - a);
   }
   
+  console.log(sortMaker([2, 3]));
+  console.log(sortMaker([4, 2]));
 
+  // Problems 4
 
 function findAddress(obj){
 
@@ -49,6 +67,23 @@ function findAddress(obj){
 
 }
 
+const adress1={
+  street: 10,
+  house: "15A",
+  society: "Earth Perfect"
+}
+console.log(findAddress(adress1));
+const adress2={
+  street: 10,
+  society: "Earth Perfect"
+}
+console.log(findAddress(adress2));
+const adress3={
+  street: 10,
+}
+console.log(findAddress(adress3));
+
+// Problems fifth
 
 
 function canPay(changeArray, totalDue) {
@@ -60,5 +95,11 @@ function canPay(changeArray, totalDue) {
   
     return totalChange >= totalDue;
   }
+
+console.log(canPay([1, 2, 5], 10)); 
+console.log(canPay([1, 5, 5], 10)); 
+console.log(canPay([2, 3, 4], 9)); 
+console.log(canPay([], 5)); 
+console.log(canPay("not an array", 5)); 
   
   
